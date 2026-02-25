b5a0c4d3867001f9251574ef18ae7dd25e79dee6
mattheo-bils <mattheobils@gmail.com>
2026-02-25 13:30:40 +0100

    suite

---
+++ asset/js/data.js
@@ -0,0 +1,162 @@
+const produits = [
+    {
+        id: 1,
+        titre: "Berserk",
+        tome: 1,
+        prix: 7.20,
+        categorie: "seinen",
+        image: "asset/img/berserk_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 2,
+        titre: "Dragon Ball",
+        tome: 35,
+        prix: 7.20,
+        categorie: "shonen",
+        image: "asset/img/dragonball_tome_35_page_de_couverture.jpg"
+    },
+    {
+        id: 3,
+        titre: "One Piece",
+        tome: 35,
+        prix: 7.20,
+        categorie: "shonen",
+        image: "asset/img/one_piece_tome_35_page_de_couverture.jpg"
+    },
+    {
+        id: 4,
+        titre: "Inazuma Eleven",
+        tome: 1,
+        prix: 7.20,
+        categorie: "kodomo",
+        image: "asset/img/inazuma_eleven_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 5,
+        titre: "Vagabond",
+        tome: 1,
+        prix: 7.20,
+        categorie: "seinen",
+        image: "asset/img/vagabond_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 6,
+        titre: "And",
+        tome: 1,
+        prix: 6.99,
+        categorie: "josei",
+        image: "asset/img/and_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 7,
+        titre: "Bleach",
+        tome: 2,
+        prix: 7.80,
+        categorie: "shonen",
+        image: "asset/img/bleach_tome_2_page_de_couverture.jpg"
+    },
+    {
+        id: 8,
+        titre: "Demon Slayer",
+        tome: 1,
+        prix: 7.40,
+        categorie: "shonen",
+        image: "asset/img/demon_slayer_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 9,
+        titre: "Doraemon",
+        tome: 2,
+        prix: 7.80,
+        categorie: "Kodomo",
+        image: "asset/img/doraemon_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 10,
+        titre: "Dr Slump",
+        tome: 1,
+        prix: 7.25,
+        categorie: "Kodomo",
+        image: "asset/img/dr_slump_tome_2_page_de_couverture.jpg"
+    },
+    {
+        id: 11,
+        titre: "Fruit Basket",
+        tome: 1,
+        prix: 7.50,
+        categorie: "shojo",
+        image: "asset/img/fruit_basket_tome_2_page_de_couverture.jpg"
+    },
+    {
+        id: 12,
+        titre: "Les Fleurs Du Passé",
+        tome: 1,
+        prix: 8.60,
+        categorie: "josei",
+        image: "asset/img/les_fleurs_du_passe_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 13,
+        titre: "Monster",
+        tome: 1,
+        prix: 8.50,
+        categorie: "seinen",
+        image: "asset/img/monster_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 14,
+        titre: "Nana",
+        tome: 1,
+        prix: 8.40,
+        categorie: "shojo",
+        image: "asset/img/nana_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 15,
+        titre: "Perfect World To Me",
+        tome: 1,
+        prix: 9.10,
+        categorie: "josei",
+        image: "asset/img/perfect_world_to_me_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 16,
+        titre: "Princess Jellyfish",
+        tome: 1,
+        prix: 9.00,
+        categorie: "josei",
+        image: "asset/img/princess_jellyfish_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 17,
+        titre: "Sailor Moon",
+        tome: 1,
+        prix: 9.50,
+        categorie: "shojo",
+        image: "asset/img/sailor_moon_tome_1_page_de_couverture.jpg"
+    },
+    {
+        id: 18,
+        titre: "Sakura Chasseuse De Carte",
+        tome: 1,
+        prix: 9.60,
+        categorie: "shojo",
+        image: "asset/img/bleach_tome_2_page_de_couverture.jpg"
+    },
+    {
+        id: 19,
+        titre: "Vinland Saga",
+        tome: 1,
+        prix: 9.30,
+        categorie: "seinen",
+        image: "asset/img/vinland_saga_tome_2_page_de_couverture.jpg"
+    },
+    {
+        id: 20,
+        titre: "Yo-kai Watch",
+        tome: 1,
+        prix: 9.10,
+        categorie: "Kodomo",
+        image: "asset/img/yo-kai_watch_tome_2_page_de_couverture.jpg"
+    },
+];
