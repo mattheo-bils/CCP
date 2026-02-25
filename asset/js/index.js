96edbfc3246e8e068738cfeba8e84b4c87b8464b
mattheo-bils <mattheobils@gmail.com>
2026-02-25 09:42:10 +0100

    carousel fait

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,30 +1,47 @@
 (function(){
-    "use stict"
+    "use strict";
     const slideTimeout = 5000;
     const prev = document.querySelector('#prev');
     const next = document.querySelector('#next');
     const $slides = document.querySelectorAll('.slide');
     let $dots;
     let intervalId;
-    let currentSlide=1;
+    let currentSlide = 0;
+
     function slideTo(index){
-        currentSlide = index >= $slides.length || index < 1 ? 0 : index;
+        if (index >= $slides.length) index = 0;
+        if (index < 0) index = $slides.length - 1;
+        currentSlide = index;
         $slides.forEach($elt => $elt.style.transform = `translateX(-${currentSlide * 100}%)`);
-        $dots.forEach(($elt, key) => $elt.classList = `dot ${key === currentSlide? 'active': 'inactive'}`);
+        $dots.forEach(($elt, key) => $elt.className = `dot ${key === currentSlide ? 'active' : 'inactive'}`);
     }
+
     function showSlide(){
         slideTo(currentSlide);
         currentSlide++;
     }
-    for (let i=1; i<= $slides.length;i++){
-        let dotClass = i == currentSlide ? 'active' : 'inactive';
-        let $dot = '<span data-slideId="${i}" class= "dot ${dotClass}"></span>';
-        document.querySelector('.carousel-dots').innerHTML += $dot;
+
+    for (let i = 0; i < $slides.length; i++){
+        let dotClass = i === currentSlide ? 'active' : 'inactive';
+        let $dot = `<span data-slideId="${i}" class="dot ${dotClass}"></span>`;
+        document.querySelector('.carousel_dots').innerHTML += $dot;
     }
+
     $dots = document.querySelectorAll('.dot');
     $dots.forEach(($elt, key) => $elt.addEventListener('click', () => slideTo(key)));
     prev.addEventListener('click', () => slideTo(--currentSlide));
     next.addEventListener('click', () => slideTo(++currentSlide));
-    intervalId = setInterval(showSlide, slideTimeout)
-    $slides
-})
+    intervalId = setInterval(showSlide, slideTimeout);
+
+    $slides.forEach($elt => {
+        let startX, endX;
+        $elt.addEventListener('mouseover', () => clearInterval(intervalId));
+        $elt.addEventListener('mouseout', () => { intervalId = setInterval(showSlide, slideTimeout); });
+        $elt.addEventListener('touchstart', (event) => { startX = event.touches[0].clientX; });
+        $elt.addEventListener('touchend', (event) => {
+            endX = event.changedTouches[0].clientX;
+            if (startX > endX) slideTo(currentSlide + 1);
+            else if (startX < endX) slideTo(currentSlide - 1);
+        });
+    });
+})()
